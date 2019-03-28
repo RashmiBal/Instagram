f3e4314c1313eaa385cc61576d7a7d97b2c1735f
RashmiBal <rashmibaliga@outlook.com>
2019-03-28 00:17:42 -0700

    FollowModel

--- models/Follow.js
+++ models/Follow.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const FollowSchema = new Schema({
     followeruser: {
       type: Schema.Types.ObjectId,
       ref: 'users'
