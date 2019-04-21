57d62069b1001ae6383319484298e4ee7145301c
RashmiBal <rashmibaliga@outlook.com>
2019-04-21 08:47:19 -0700

    NavbarFont

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
  class Navbar extends Component {
   render() {
+    const isAuthenticated = false; // TODO: read value from props
+
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Logout
+          </Link>
+        </li>
+        </ul>);
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+
     return (
-<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
+        <Link className="navbar-brand" to="/">
+            Instagram
+          </Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -12,13 +42,15 @@ import React, { Component } from 'react'
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html"> Instagram
-                </a>
+                <Link className="nav-link" to="/">
+                <i class="fas fa-home"></i>
+                </Link>
               </li>
             </ul>
+            {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
-       </nav> 
+      </nav> 
     )
   }
 }
