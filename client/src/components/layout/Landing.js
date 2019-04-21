9a652a520c0300a3b0238a2d09a171b2537178dd
RashmiBal <rashmibaliga@outlook.com>
2019-04-21 00:46:08 -0700

    LandingPage

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
  class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing page</h1>
-      </div>
-    )
+            <div className="landing">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <h1 className="display-3 mb-4">Stellar Instagram</h1>
+                    <p className="lead">
+                      {' '}
+                      Share your passion and connect with others
+                    </p>
+                    <hr />
+                    <Link to="/register" className="btn btn-lg btn-light mr-2">
+                      Sign Up
+                    </Link>
+                    <Link to="/login" className="btn btn-lg btn-light">
+                      Login
+                    </Link>
+                  </div>
+                </div>
+              </div>
+          </div>
+    );
   }
 }
 
