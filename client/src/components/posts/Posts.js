1db2dfa40a308ff01dea5b14cde841e625b724bb
RashmiBal <rashmibaliga@outlook.com>
2019-04-26 10:26:24 -0700

    VerifiedPostsWorkFromTeamworkApr25

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import PostForm from "./PostForm";
 import PostFeed from "./PostFeed";
 import { getPosts } from '../../actions/postActions';
 import {connect} from 'react-redux'; 
@@ -22,7 +23,7 @@ class Posts extends Component {
           <div className="row">
             <div className="col-md-12">
             posts
-              {/* <PostForm /> */}
+              <PostForm />
               <PostFeed posts={posts} /> 
             </div>
           </div>
