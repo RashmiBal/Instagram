cfb0707d92eed4475536b5a143335de77b02f126
Sergiu <nixigg@gmail.com>
2019-04-26 19:08:23 -0700

    Post Form added

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,14 +1,34 @@
 import axios from 'axios';
 
 import {
-  //ADD_POST,
-  //GET_ERRORS,
+  ADD_POST,
+  GET_ERRORS,
   //CLEAR_ERRORS,
   GET_POSTS,
   //GET_POST,
   //POST_LOADING,
  // DELETE_POST
 } from './types';
+
+// Add Post
+export const addPost = postData => dispatch => {
+  //dispatch(clearErrors());
+  axios
+    .post('/api/posts', postData)
+    .then(res =>
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 
 // Get Posts
 export const getPosts = () => dispatch => {
