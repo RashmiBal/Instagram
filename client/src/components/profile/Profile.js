93e24e495a930a85ad1d28b210262553c3766c16
RashmiBal <rashmibaliga@outlook.com>
2019-04-26 23:56:42 -0700

    ProfileAndFollowUI

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,54 +1,79 @@
-import React, { Component } from 'react'
-import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Followers from "./Followers";
 import Followings from "./Followings";
 
 class Profile extends Component {
-    componentDidMount() {
-        this.props.getCurrentProfile();
-      }    
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
 
-    onDeleteClick(e) {
-        this.props.deleteAccount();
-      }
+  onDeleteClick(e) {
+    this.props.deleteAccount();
+  }
 
   render() {
-    const { user } = this.props.auth; 
+    const { user } = this.props.auth;
     const { profile } = this.props.profile;
 
     let dashboardContent;
     let profilefound = 0;
 
-    if (profile !== null){
-        profilefound = 1;
+    if (profile !== null) {
+      profilefound = 1;
     }
 
-    if (profilefound > 0) 
-    {
-    dashboardContent = (
+    if (profilefound > 0) {
+      dashboardContent = (
         <div>
-          <p className="lead text-muted">
-            Welcome {user.name}
-          </p>
-          <div>{profile.title} {profile.website} {profile.bio}</div>
-          <div className="btn-group mb-4" role="group">
-            <Link to="/edit-profile" className="btn btn-light">
-                <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
-            </Link>
-            <Link className="btn btn-light">
-                Followers {' '} {profile.follower.length}
-            </Link>
-            <Link className="btn btn-light">
-                Following {' '} {profile.following.length}
-            </Link>
+          <p className="display-4 text-muted">Welcome {user.name}</p>
+          <div>
+            Title: {profile.title} <br />
+            WebSite: {profile.website} <br />
+            Bio: {profile.bio}
+          </div>
+          <hr />
+          <div>
+            <ul className="nav nav-pills">
+              <li>
+                <a
+                  href="#followers"
+                  data-toggle="pill"
+                  className="btn btn-light"
+                >
+                  Followers {profile.follower.length}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#following"
+                  data-toggle="pill"
+                  className="btn btn-light"
+                >
+                  Following {profile.following.length}
+                </a>
+              </li>
+              <li>
+                <Link to="/edit-profile" className="btn btn-light">
+                  <i className="fas fa-user-circle text-info mr-1" /> Edit
+                  Profile
+                </Link>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div className="tab-pane fade in active" id="followers">
+                <Followers followers={profile.follower} />
+              </div>
+              <div className="tab-pane fade" id="following">
+                <Followings followings={profile.following} />
+              </div>
             </div>
-            <Followers followers={profile.follower} />
-            <Followings followings={profile.following} />
-
-          <div style={{ marginBottom: '60px' }} />
+          </div>
+          <hr />
+          <div style={{ marginBottom: "60px" }} />
           <button
             onClick={this.onDeleteClick.bind(this)}
             className="btn btn-danger"
@@ -57,15 +82,14 @@ class Profile extends Component {
           </button>
         </div>
       );
-    }
-    else {
-        dashboardContent = (
-            <div>
-              <p className="lead text-muted">Welcome</p>
-              <p>You have not yet setup a profile, please add some info</p>
-              Create Profile link
-            </div>
-          );
+    } else {
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome</p>
+          <p>You have not yet setup a profile, please add some info</p>
+          Create Profile link
+        </div>
+      );
     }
 
     return (
@@ -73,7 +97,6 @@ class Profile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4">Dashboard</h1>
               {dashboardContent}
             </div>
           </div>
@@ -84,18 +107,18 @@ class Profile extends Component {
 }
 
 Profile.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
-    deleteAccount: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired
-  };
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
-  const mapStateToProps = state => ({
-    profile: state.profile,
-    auth: state.auth
-  });
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
 
-  export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
-    Profile
-  );
-
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Profile);
