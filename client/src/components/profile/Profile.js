6c4bc8a2a8654b27115d6f88bbe4c46422dba5df
GitSari <sarithababu@gmail.com>
2019-04-29 15:18:41 -0700

    reverted the changes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,9 +3,8 @@ import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import Spinner from "../common/Spinner";
-// import Followers from "./Followers";
-// import Followings from "./Followings";
+import Followers from "./Followers";
+import Followings from "./Followings";
 
 class Profile extends Component {
   componentDidMount() {
@@ -21,57 +20,83 @@ class Profile extends Component {
     const { profile } = this.props.profile;
 
     let dashboardContent;
-    //let profilefound = 0;
+    let profilefound = 0;
 
-    // if (profile !== null) {
-    //   profilefound = 1;
-    // }
-
-  
-
-    if (profile === null) //loading 
-    {
-      dashboardContent = <Spinner />;
-    } else {
-      // Check if logged in user has profile data
-      if (Object.keys(profile).length > 0) {
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">
-              Welcome{" "}
-              <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
-            </p>
-         
-            <div style={{ marginBottom: "60px" }} />
-            <button
-              onClick={this.onDeleteClick.bind(this)}
-              className="btn btn-danger"
-            >
-              Delete My Account
-            </button>
-          </div>
-        );
-      } else {
-        // User is logged in but has no profile
-        dashboardContent = (
-          <div>
-            <p className="lead text-muted">Welcome {user.name}</p>
-            <p>You have not yet setup a profile, please add some info</p>
-            <Link to="/create-profile" className="btn btn-lg btn-info">
-              Create Profile
-            </Link>
-          </div>
-        );
-      }
+    if (profile !== null) {
+      profilefound = 1;
     }
 
+    if (profilefound > 0) {
+      dashboardContent = (
+        <div>
+          <p className="display-4 text-muted">Welcome {user.name}</p>
+          <div>
+            Title: {profile.title} <br />
+            WebSite: {profile.website} <br />
+            Bio: {profile.bio}
+          </div>
+          <hr />
+          <div>
+            <ul className="nav nav-pills">
+              <li>
+                <a
+                  href="#followers"
+                  data-toggle="pill"
+                  className="btn btn-light"
+                >
+                  Followers {profile.follower.length}
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#following"
+                  data-toggle="pill"
+                  className="btn btn-light"
+                >
+                  Following {profile.following.length}
+                </a>
+              </li>
+              <li>
+                <Link to="/edit-profile" className="btn btn-light">
+                  <i className="fas fa-user-circle text-info mr-1" /> Edit
+                  Profile
+                </Link>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div className="tab-pane fade in active" id="followers">
+                <Followers followers={profile.follower} />
+              </div>
+              <div className="tab-pane fade" id="following">
+                <Followings followings={profile.following} />
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div style={{ marginBottom: "60px" }} />
+          <button
+            onClick={this.onDeleteClick.bind(this)}
+            className="btn btn-danger"
+          >
+            Delete My Account
+          </button>
+        </div>
+      );
+    } else {
+      dashboardContent = (
+        <div>
+          <p className="lead text-muted">Welcome</p>
+          <p>You have not yet setup a profile, please add some info</p>
+          Create Profile link
+        </div>
+      );
+    }
 
     return (
       <div className="dashboard">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4">Profile</h1>
               {dashboardContent}
             </div>
           </div>
